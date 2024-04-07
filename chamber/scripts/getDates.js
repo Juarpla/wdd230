5ce08f8f40d48a982fcb06d782f9c9a10b8f1666
Juan Plasencia <jplasencia.dev@outlook.com>
2024-04-07 15:01:31 -0500

    .

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -8,6 +8,3 @@ copyright.innerText = `© ${currentYear} Juan Arturo Plasencia`;
 
 let modified = document.querySelector("#lastModified");
 modified.innerText = `Last Modification: ${lastModified}`;
-
-let events = document.querySelector("#titleEvents");
-events.innerText = `Events in ${currentMonth}`;
